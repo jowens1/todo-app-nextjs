cb13f0a87d1ad324c2206f0b754f652922254adc
jowens1 <justinowens328@gamail.com>
2022-08-26 12:37:01 -0700

    fixes for prod auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Container from '../components/container';
 import Card from '../components/card';
 import Tile from '../components/tile';
 import Layout from '@/components/layout';
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
@@ -19,6 +19,15 @@ const Home: NextPage = () => {
           </Card>
         ) : (
           <Card>
+            <div className="flex items-center">
+              <Tile
+                linkPath="/api/auth/signout"
+                imgPath="/logout"
+                type="icon"
+                height={16}
+                width={16}
+              />
+            </div>
             <div className="flex justify-center rounded-xl w-52 shadow-frame">
               <Tile imgPath="/checklist" linkPath="/todos" />
               <Tile imgPath="/notes2" linkPath="/todos" />
